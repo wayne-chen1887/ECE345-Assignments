7d031f07099c5556fccb7b4a126d433dc0880c03
Wayne <wayne.cwc@gmail.com>
2021-01-28 22:03:02 -0500

    ready for submission

--- A1/QuickSort.py
+++ A1/QuickSort.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 
 # QUICKSORT
 # Worst Case - Theta(n^2)
@@ -10,59 +10,59 @@ import math
 import matplotlib.pyplot as plt
 
 sys.setrecursionlimit(10**9)
-sysarg = False
-test = True
 
-if sysarg:
-    InputFile = sys.argv[0]
-else:
-    InputFile = 'a1.large'
+Submission = True
+InputFile = sys.argv[1]
 
 # Storing Data as nested list, where key is element 0 of nested list
-Data = []
+Data = dict()
 with open(InputFile + '.csv', 'r') as csvfile:
     csv_reader = csv.reader(csvfile)
     for row in csv_reader:
-        Data.append(int(row[0]))
+        Data[int(row[0])] = list()
+        for i in row[1:]:
+            Data[int(row[0])].append(i)
+Arr = list(Data)
 
 
-def DetermineInputSizeList(Data, buckets):
+def DetermineInputSizeList(Arr, buckets):
     InputSize = []
-    i = int(len(Data)/buckets)
-    for idx, value in enumerate(Data):
+    i = int(len(Arr)/buckets)
+    for idx, value in enumerate(Arr):
         if idx % i == 0 and idx != 0:
             InputSize.append(idx)
-        if idx == len(Data) - 1:
+        if idx == len(Arr) - 1:
             InputSize.append(idx)
     return InputSize
 
 
-def Partition(Data, p, r):
-    pivot = Data[r]
+def Partition(Arr, p, r):
 
+    pivot = Arr[r]
     i = p - 1
 
     for j in range(p, r):
-        if Data[j] <= pivot:
+        if Arr[j] <= pivot:
             i += 1
-            Data[i], Data[j] = Data[j], Data[i]
-    Data[i+1], Data[r] = Data[r], Data[i+1]  # Swap Elements
+            Arr[i], Arr[j] = Arr[j], Arr[i]
+    # Swap Elements
+    Arr[i+1], Arr[r] = Arr[r], Arr[i+1]
     return (i+1)  # Return element q
 
 
-def QuickSort(Data, p, r):
+def QuickSort(Arr, p, r):
 
     if (p < r):
-        q = Partition(Data, p, r)
-        QuickSort(Data, p, q - 1)
-        QuickSort(Data, q + 1, r)
+        q = Partition(Arr, p, r)
+        QuickSort(Arr, p, q - 1)
+        QuickSort(Arr, q + 1, r)
 
 
-def TimeAlgorithm(Data, InputSize, p, r):
+def TimeAlgorithm(Arr, InputSize, p, r):
     ExecTime = []
     for i in InputSize:
         StartTime = time.time()
-        QuickSort(Data[0:i], p, i-1)
+        QuickSort(Arr[0:i], p, i-1)
         ExecTime.append(time.time() - StartTime)
     return ExecTime
 
@@ -76,15 +76,18 @@ def Graph(InputSize, ExecTime):
 
 
 p = 0
-r = len(Data) - 1
+r = len(Arr) - 1
 
-if test:
-    QuickSort(Data, p, r)
-    SortStatus = all(Data[i] <= Data[i+1]
-                     for i in range(len(Data)-1))  # Check if Sorted
-    print(f'Is the Dataset Sorted: {SortStatus}')
-else:
-    print(f'Running QuickSort on Input Size of {len(dataset)}')
-    InputSize = DetermineInputSizeList(dataset, 5)
-    ExecTime = TimeAlgorithm(dataset, InputSize, p, r)
+if Submission:  # Submission flag
+    print(f'Running QuickSort on Input Size of {len(Arr)}')
+    InputSize = DetermineInputSizeList(Arr, 5)
+    ExecTime = TimeAlgorithm(Arr, InputSize, p, r)
     Graph(InputSize, ExecTime)
+else:
+    QuickSort(Arr, p, r)
+    for i in Arr:
+        Data[i].insert(0, i)
+        print(Data[i])
+    SortStatus = all(Arr[i] <= Arr[i+1]
+                     for i in range(len(Arr)-1))  # Check if Sorted
+    print(f'Is the Dataset Sorted: {SortStatus}')
